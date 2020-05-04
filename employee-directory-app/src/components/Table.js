32ba56a7bcac341d4d27d34ed0e9ab774144dd83
PROX-E <lichlord18@gmail.com>
2020-05-04 15:18:06 -0600

    update table.js

--- employee-directory-app/src/components/Table.js
+++ employee-directory-app/src/components/Table.js
@@ -3,3 +3,15 @@ import { useGet } from "../hooks/API.js"
 import "./Table.css"
 import Button from "./Button.js"
 import { EmployeeContext } from './EmployeeContext.js';
+
+
+function Table() {
+    
+    const [url] = useState("https://randomuser.me/api/?results=10")
+    const { sortFunc } = useGet(url);
+    // contains the employees to display in the table
+    const { displayedEmployees} = useContext(EmployeeContext)
+
+    
+}
+export default Table;
