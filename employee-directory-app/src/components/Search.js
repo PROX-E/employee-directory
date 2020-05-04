30e8a33575d826aa1334eff678aa2f4ea8850a60
PROX-E <lichlord18@gmail.com>
2020-05-04 14:18:26 -0600

    update search function comp

--- employee-directory-app/src/components/Search.js
+++ employee-directory-app/src/components/Search.js
@@ -6,6 +6,13 @@ function Search(){
     const [search, setSearch] = useState("")
     const {employees, setDisplayedEmployees} = useContext(EmployeeContext)
     
+    function updateSearch({target}){
+        // just using for readability
+        const searchTerm = target.value
+        // used for updating the value in the search input, so it always equals exactly what is saved in state
+        setSearch(searchTerm)
+        
+    }
     // every letter entered triggers the onChange event.
     return(
        <input type="text" onChange= {updateSearch} value={search}></input>
