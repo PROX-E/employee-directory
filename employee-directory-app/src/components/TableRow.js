8f60eeedc5314324f09f2d00f29de5ad013bd5c3
PROX-E <lichlord18@gmail.com>
2020-05-04 14:15:53 -0600

    update TableRow js component

---
+++ employee-directory-app/src/components/TableRow.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./TableRow.css"
+
+function TableRow(){
+    return(
+        <tr>
+            <td>Luke</td>
+            <td>O'Henry</td>
+            <td>21404391</td>
+            <td>Male</td>
+            <td>Luke@sampleEmail.com</td>
+            <td>26</td>
+        </tr>
+    )
+}
+export default TableRow;
