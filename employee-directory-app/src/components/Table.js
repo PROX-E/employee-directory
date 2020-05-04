8838c30c59504c07318feb7ca38892f09e177956
PROX-E <lichlord18@gmail.com>
2020-05-04 15:23:41 -0600

    render 15 results

--- employee-directory-app/src/components/Table.js
+++ employee-directory-app/src/components/Table.js
@@ -7,7 +7,7 @@ import { EmployeeContext } from './EmployeeContext.js';
 
 function Table() {
     
-    const [url] = useState("https://randomuser.me/api/?results=10")
+    const [url] = useState("https://randomuser.me/api/?results=15")
     const { sortFunc } = useGet(url);
     // contains the employees to display in the table
     const { displayedEmployees} = useContext(EmployeeContext)
