e7576b92865fcfd4c02b503810230d2d3d08769a
PROX-E <lichlord18@gmail.com>
2020-05-04 14:10:45 -0600

    update navbar.js

--- employee-directory-app/src/components/Navbar.js
+++ employee-directory-app/src/components/Navbar.js
@@ -3,6 +3,14 @@ import Search from "./Search.js"
 import "./Navbar.css"
 
 function Navbar(){
-    
+    return(
+        <div className="navbar">
+            <div className="logo bold">Employee Directory</div>
+            <div className="descriptor">
+                <div className="bold">Employee Search: </div>
+                
+            </div>
+        </div>
+    )
 }
 export default Navbar;
