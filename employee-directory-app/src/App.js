1ccb12a65477463fc4d20ed4e011df4db733c694
PROX-E <lichlord18@gmail.com>
2020-05-04 14:59:13 -0600

    Restructure app.js to use imported components

--- employee-directory-app/src/App.js
+++ employee-directory-app/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// component imports
+import Navbar from "./components/Navbar"
+import Table from "./components/Table"
+import { EmployeeContext } from "./components/EmployeeContext"
 
 function App() {
+  // State function definitions
+  const [employees, setEmployees] = useState([]);
+  const [displayedEmployees, setDisplayedEmployees] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <EmployeeContext.Provider value={{employees, setEmployees, displayedEmployees, setDisplayedEmployees}}>
+        <Navbar />
+        <Table />
+      </EmployeeContext.Provider>
+
     </div>
   );
 }
