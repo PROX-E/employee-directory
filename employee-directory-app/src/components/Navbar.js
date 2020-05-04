23717cc7344a5210441881f4d9a9ea55f2750c6a
PROX-E <lichlord18@gmail.com>
2020-05-04 14:21:12 -0600

    update navbar with search component

--- employee-directory-app/src/components/Navbar.js
+++ employee-directory-app/src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Search from "./Search.js"
 
-
 function Navbar(){
     return(
         <div className="navbar">
             <div className="logo bold">Employee Directory</div>
             <div className="descriptor">
                 <div className="bold">Employee Search: </div>
-                
+                <Search />
             </div>
         </div>
     )
